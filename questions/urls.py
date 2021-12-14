37515d571ebcb51a310914d8e58b3bcb9511ca98
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-12-14 16:27:28 -0500

    added rest framework urls to urls.py

--- questions/urls.py
+++ questions/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('questions/', views.QuestionList.as_view(), name="question_list")
+    path('api-auth/', include('rest_framework.urls')),
+    path('questions/', views.QuestionList.as_view(), name="question_list"),
 ]
