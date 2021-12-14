628f2302afbcb286feb87777719e194404e23ff1
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-12-14 14:20:05 -0500

    made migrations, migrated, made models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    image_url = models.CharField(max_length=100, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    
+    def __repr__(self):
+        return f"<User username={self.username}>"
+
+    def __str__(self):
+        return self.username
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey('User', on_delete=models.DO_NOTHING, related_name="questions")
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    favorited = models.ManyToManyField('User', related_name="fav_questions", blank=True)
+    answer = models.BooleanField(default=False)
+
+class Answer(models.Model):
+    body = models.TextField()
+    author = models.ForeignKey('User', on_delete=models.DO_NOTHING, related_name="answers")
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name="answers")
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    favorited = models.ManyToManyField('User', related_name="fav_answers", blank=True)
