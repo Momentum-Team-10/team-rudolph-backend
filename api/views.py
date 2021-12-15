1ba6c246002f97f5de1c471fed618bdc8e98ec0b
James Andrews <jbandrew14@gmail.com>
2021-12-14 19:16:21 -0500

    changed parent class to ListCreateAPIView and overrided perform_create method

--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from .models import Question
 from .serializers import QuestionSerializer
 
 # Create your views here.
 
-class QuestionList(ListAPIView):
+class QuestionList(ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
