b451cca0f903043965037aabde213d5485e9fc4c
James Andrews <jbandrew14@gmail.com>
2021-12-14 20:19:31 -0500

    added AnswerSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,35 @@
 from rest_framework import serializers
-from .models import Question
+from .models import Answer, Question
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = (
+            'pk',
+            'body',
+            'author',
+            'question',
+            'votes',
+            'created_at',
+            'favorited',
+        )
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    # answers = AnswerSerializer(many=True, read_only=True)
+    answers = AnswerSerializer(many=True, read_only=True)
     author = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Question
         fields = (
+            'pk',
             'title',
             'body',
             'author',
             'votes',
+            'answers',
             'created_at',
             'favorited',
             'answered',
         )
+        
