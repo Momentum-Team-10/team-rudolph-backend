13f2ec8dd754135e3cf5ee919c9897fbdc3b71f8
James Andrews <jbandrew14@gmail.com>
2021-12-20 10:26:37 -0500

    renamed permission IsQuestionAuthor to IsAuthor

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsQuestionAuthor(permissions.BasePermission):
+class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         methods = ('DELETE', 'PATCH')
         if request.method in methods:
